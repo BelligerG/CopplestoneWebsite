c5dd6bc9c9e36f1918da9182fe94bef67810f860
Chris Sampson <chris.m.sampson@gmail.com>
2019-12-09 15:03:20 +0000

    Making the Gallery scaleable

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-var slideIndex = 0;
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
-    var i;
-    var imgs = document.getElementsByClassName("img-container");
-    if (n < 0) { n = imgs.length-1; }
-    if (n > imgs.length-1) { n = 0; }
-    slideIndex = n;
-    for (i = 0; i < imgs.length; i++) {
-        imgs[i].style.display = "none";
-    }
-    imgs[n].style.display = "inline";
-}
-
-function next() {
-    ++slideIndex;
-    showSlides(slideIndex);
-}
-
-function prev() {
-    --slideIndex;
-    showSlides(slideIndex);
-}
-
-
-function openModal() {
-    var id = document.getElementById("modalScreen");
-    id.style.display = "inline";
-}
-
-function closeModal() {
-    var id = document.getElementById("modalScreen");
-    id.style.display = "none";
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+    slides[slideIndex-1].style.display = "block";
 }

a67689bffe5f71e06eeebc683a0995deced4a010
Chris Sampson <chris.m.sampson@gmail.com>
2019-12-10 13:28:18 +0000

    First public site commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function closeModal() {
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
+/*showSlides(slideIndex);*/
 
 // Next/previous controls
 function plusSlides(n) {
@@ -24,15 +24,10 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  else if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
   }
     slides[slideIndex-1].style.display = "block";
 }

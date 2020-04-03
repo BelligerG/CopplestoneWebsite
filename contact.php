f98581b7c7b69a6e82b14fd514055a3929a4b1e1
Chris Sampson <chris.m.sampson@gmail.com>
2020-04-03 19:52:03 +0100

    Fixed security issues PHP

--- contact.php
+++ contact.php
@@ -1,9 +1,8 @@
 <?php
-//https://supunkavinda.blog/php/contact-form
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	if (empty($_POST['eAddress'])) {
+    if (empty($_POST['eAddress'])) {
 		$emailError = 'Email is empty';
 	} else {
 		$eAddress =  str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['eAddress']);
@@ -38,117 +37,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$subjectError = 'Invalid message';
 		}
 	}
-
-	if (empty($emailError) && empty($nameError) && empty($queryError) && empty($subjectError)) {
+    
+    if (empty($emailError) && empty($nameError) && empty($queryError) && empty($subjectError)) {
 		$date = date('j, F Y h:i A');
 		$siteAddress = "admin@copplestonechurch.org.uk";
+        $recipient = '';
 	
 
 		if($queryType == "leadership"){
-        		$recipient = "johnwiltshireringsash@gmail.com";
+        		$recipient = "Leadership < johnwiltshireringsash@gmail.com >";
         	}
         	elseif($queryType == "youthWork"){
-        		$recipient = " Children and Youth < markwilliamsccy@gmail.com >";
+        		$recipient = "Children and Youth < markwilliamsccy@gmail.com >";
         	}
         	elseif($queryType == "magazine"){
-        		$recipient = "lunn.family4@gmail.com";
+        		$recipient = "Enlightener < lunn.family4@gmail.com >";
         	}
         	elseif($queryType == "safeguard"){
-        		$recipient = "hjpetherick@hotmail.co.uk";
+        		$recipient = "Safeguard < hjpetherick@hotmail.co.uk >";
         	}
         	elseif($queryType == "notice"){
         		$recipient = " Notices < davidandeunice@hotmail.co.uk >";
         	}
         	elseif($queryType == "lifespring"){
-        		$recipient = "seannicschofield@yahoo.co.uk";
+        		$recipient = "Lifespring < seannicschofield@yahoo.co.uk >";
         	}
         	elseif($queryType == "website"){
-        		//$recipient = " Lindsey < lindsey2@talk21.com >";
-        		$recipient = "chris.m.sampson@gmail.com";
+        		$recipient = "Website Issue < chris.m.sampson@gmail.com >";
 		}
+        
+        $message = "
+			<html>
+			<head>
+				<title>You have been sent the following message via Copplestone Church\'s contact form.</title>
+			</head>
+			<body>
+				<div style=\"padding:20px;\">
+					Date: $date
+					<br>
+                    <br>
+					Name: $name
+					<br>
+                    <br>
+					Email Address: $eAddress
+					<br>
+                    <br>
+					Message: $subject
+				</div>
+			</body>
+			</html>
+			";
+
+        
+        $headers = "From: Admin < admin@copplestonechurch.org.uk >" . "\r\n" .
+			"Reply-To: $eAddress" . "\r\n" .
+            "X-Sender: admin < $siteAddress >" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion() . "\r\n" . 
+            "X-Priority: 1" . "\r\n" . 
+            "Return-Path: chris.m.sampson@gmail.com" . "\r\n" .
+			"MIME-Version: 1.0" . "\r\n" .
+			"Content-Type: text/html; charset=ISO-8859-1" . "\r\n";
 
 
-	}
-
-
-    $name = "";
-    $eAddress = "";
-    $queryType = "";
-    $subject = "";
-    $siteAddress = "admin@copplestonechurch.org.uk";
-    $boundary = str_replace(" ", "", date('l jS \of F Y h i s A'));
-    
-    if(isset($_POST['name'])) {
-        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    } 
-    
-    if(isset($_POST['eAddress'])) {
-        $eAddress = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['eAddress']);
-        $eAddress = filter_var($eAddress, FILTER_VALIDATE_EMAIL);
+        if(mail($recipient, "Query from website", wordwrap($message), $headers)) {
+            echo "<p>Thank you for contacting us, $name. We will respond ASAP.</p>";
+        } else {
+            echo '<p>We are sorry, the email did not go through, please try again.</p>';
+        }
+    } elseif(isset($emailError) || (isset($nameError)) || (isset($queryError)) || (isset($subjectError))) {
+        echo isset($emailError) ? $emailError . '<br>' : '' ;
+        echo isset($nameError) ? $nameError . '<br>' : '' ;
+        echo isset($queryError) ? $queryError . '<br>' : '' ;
+        echo isset($subjectError) ? $subjectError . '<br>' : '' ;
     }
-    
-    if(isset($_POST['queryType'])) {
-        $queryType = filter_var($_POST['queryType'], FILTER_SANITIZE_STRING);
-    }
-    
-    if(isset($_POST['subject'])) {
-        $subject = htmlspecialchars($_POST['subject']);
-    }
-    
-    if($queryType == "leadership"){
-        $recipient = "johnwiltshireringsash@gmail.com";
-    }
-    elseif($queryType == "youthWork"){
-        $recipient = " Children and Youth < markwilliamsccy@gmail.com >";
-    }
-    elseif($queryType == "magazine"){
-        $recipient = "lunn.family4@gmail.com";
-    }
-    elseif($queryType == "safeguard"){
-        $recipient = "hjpetherick@hotmail.co.uk";
-    }
-    elseif($queryType == "notice"){
-        $recipient = " Notices < davidandeunice@hotmail.co.uk >";
-    }
-    elseif($queryType == "lifespring"){
-        $recipient = "seannicschofield@yahoo.co.uk";
-    }
-    elseif($queryType == "website"){
-        $recipient = " Lindsey < lindsey2@talk21.com >";
-        //$recipient = "chris.m.sampson@gmail.com";
-    }
-    
-    $message = '<div>You have been sent the following message via Copplestone Church\'s contact form.<br /><br />Sender: ' . $eAddress . '<br /><br />Please do not reply directly to this message, but to the sender (above).<br /><br /> Message:</div>';
-    
-    $message .= '<div>' . $subject . '<br /><br /></div>';
-    $message .= '<div>End of message. <br />Name of Sender: ' . $name . '</div>';
-    
-    $headers = '';
-    $headers .= 'From: ' . $siteAddress . "\r\n" . 'Reply-To: ' . $siteAddress . "\r\n";
-    $headers .= 'Return-Path: ' . $siteAddress . "\r\n";
-    $headers .= 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n";
-
-
-    if($queryType == "website"){
-       $headers = '';
-       $headers .= 'From: admin < ' . $siteAddress . " >\n" . 'Reply-To: admin < ' . $siteAddress . " >\n";
-       $headers .= 'X-Sender: admin < ' . $siteAddress . " >\n";
-       $headers .= "Cc: testsite < chris.m.sampson@gmail.com >\n";
-       $headers .= 'X-Mailer: PHP/' . phpversion();
-       $headers .= "X-Priority: 1\n"; // Urgent message!
-       $headers .= "Return-Path: chris.m.sampson@gmail.com\n"; // Return path for errors
-       $headers .= "MIME-Version: 1.0\r\n";
-       $headers .= "Content-Type: text/html; charset=iso-8859-1\n";
-    }
-    
-    
-    if(mail($recipient, "Query from website", wordwrap($message), $headers)) {
-        echo "<p>Thank you for contacting us, $name. We will respond ASAP.</p>";
-    } else {
-        echo '<p>We are sorry, the email did not go through, please try again.</p>';
-    }
-    
 } else {
     echo '<p>Something went wrong</p>';
 }
